99d326622558487bbd6a3c3ee004ca463356fc65
bukharov <dilmurod.bukharov@gmail.com>
2019-12-01 23:32:32 -0500

    tried to connect api to my site

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hey")
+const url = "https://newsapi.org/v2/everything?q=apple&from=2019-12-01&to=2019-12-01&sortBy=popularity&apiKey=API_KEY";
+fetch(url, {
+    headers: {
+        "news-api-key": "7d3ee15c957a409e9307d7099b89f646"
+    }
+})
+.then(res => res.json())
+.then(res => {
+    console.log(res);
+});
