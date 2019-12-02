fd0facb1348f6f8e77a4286ef6451ee3fd7e446d
bukharov <dilmurod.bukharov@gmail.com>
2019-12-01 23:39:21 -0500

    connected api to my site

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const url = "https://newsapi.org/v2/everything?q=apple&from=2019-12-01&to=2019-12-01&sortBy=popularity&apiKey=API_KEY";
+const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=7d3ee15c957a409e9307d7099b89f646";
 fetch(url, {
-    headers: {
-        "news-api-key": "7d3ee15c957a409e9307d7099b89f646"
+    header: {
+        "X-Api-Key": "7d3ee15c957a409e9307d7099b89f646"
     }
 })
 .then(res => res.json())
